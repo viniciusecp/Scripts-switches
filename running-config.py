85eb99ecfd19857f56c58cefd4c7e4cdb2b7f2d4
Vinicius Silva <viniciusfs.ecp@gmail.com>
2018-11-28 10:50:41 -0200

    Adicionado arquivos running-config exclusivos para cada modelo de switch

--- running-config.py
+++ running-config.py
@@ -1,24 +1,77 @@
 import os
 
-#fpath = '/home/vinicius/Desktop/'
-fpath = input('Caminho da pasta: ')
-
-caminhos = [os.path.join(fpath, nome) for nome in os.listdir(fpath)]
-arquivos = [arq for arq in caminhos if os.path.isfile(arq)]
-txt = [arq for arq in arquivos if arq.lower().endswith(".txt")]
-
-for path in txt:
+def formatarIOS(path):
     fname = path
     file = open(fname, 'r')
     text = file.read()
     file.close()
 
-    text = text.replace('\\n', '\n' )
-    text = text.replace('\\t', '\t' )
-    text = text.split("Current configuration")[2]
-    text = "Current configuration" + text
-    text = text[:len(text)-3]
+    try:
+        text = text.replace('\\n', '\n' )
+        text = text.replace('\\t', '\t' )
+        text = text.split("Current configuration")[2]
+        text = "Current configuration" + text
+        text = text[:len(text)-3]
+    except Exception as e:
+        raise
 
     file = open(fname, 'w')
     file.write(text)
     file.close()
+
+def formatarNexus(path):
+    fname = path
+    file = open(fname, 'r')
+    text = file.read()
+    file.close()
+
+    try:
+        text = text.replace('\\n', '\n' )
+        text = text.replace('\\t', '\t' )
+        text = text.split("!Command: show running-config")[2]
+        text = "!Command: show running-config" + text
+        text = text[:len(text)-3]
+    except Exception as e:
+        raise
+
+    file = open(fname, 'w')
+    file.write(text)
+    file.close()
+
+def formatarDell(path):
+    fname = path
+    file = open(fname, 'r')
+    text = file.read()
+    file.close()
+
+    try:
+        text = text.replace('\\n', '\n' )
+        text = text.replace('\\t', '\t' )
+        text = text.split("Current Configuration")[2]
+        text = "Current Configuration" + text
+        text = text[:len(text)-3]
+    except Exception as e:
+        raise
+
+    file = open(fname, 'w')
+    file.write(text)
+    file.close()
+
+main():
+    #fpath = '/home/vinicius/Desktop/'
+    fpath = input('Caminho da pasta: ')
+
+    caminhos = [os.path.join(fpath, nome) for nome in os.listdir(fpath)]
+    arquivos = [arq for arq in caminhos if os.path.isfile(arq)]
+    ios = [arq for arq in arquivos if arq.lower().endswith("ios.txt")]
+    nexus = [arq for arq in arquivos if arq.lower().endswith("nexus.txt")]
+    dell = [arq for arq in arquivos if arq.lower().endswith("dell.txt")]
+
+    for path in ios:
+        formatarIOS(path)
+    for path in nexus:
+        formatarNexus(path)
+    for path in dell:
+        formatarDell(path)
+
+main()
